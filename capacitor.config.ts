f4d6780eb51325216ae5dec7499c220d3fdd0770
xDustzz <xdustine@gmail.com>
2026-03-16 12:47:19 +0700

    final: web disabled, focus on app release

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.xDustzz.app',
-  appName: 'xDustAI',
-  webDir: 'dist'
-};
-
-export default config;
